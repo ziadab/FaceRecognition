203581487464c906a6b79731172107987d0f2d16
Hirokazo Nagata <zeus@localhost.localdomain>
2018-07-08 16:52:44 +0100

    Add Comment for Others To Know The Stracture Of The TOOLS

--- Face.py
+++ Face.py
@@ -4,32 +4,60 @@ import numpy as np
 import cv2, pickle
 
 
-
+#Creat a face Recognizer
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+#Load the haarcascade frontalface Classifier
 face = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 #eye = cv2.CascadeClassifier("haarcascade_eye.xml")
+
+#Read the trainner.yml model that generate using trainner.py
 recognizer.read('trainner.yml')
 
+#Declare an empty label dict
 labels = {}
+
+#load the names that in the video 
 with open("labels.pkl","rb") as f:
     labelsH = pickle.load(f)
     labels = {v:k for k,v in labelsH.items()}
 
+############ Warning  #####################
 cap = cv2.VideoCapture("OverMyHead.mp4")
+#for this line if you want use the input of the video is camera live steam 
+#put this code :
+#cap = cv2.VideoCapture(0)
+#And if you wnat an other video just replace the argument in cap 
+#like this:
+#cap = cv2.VideoCapture("myVideo.mp4")
+###########################################
 
 #i = 0
 
+#Creat a loop
 while True:
+    #read from the video/camera
     ret, img = cap.read()
+
+    #convert the color of img to gray
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #detect faces in the picture usingg the haarcascade frontalface Classifier
     faces = face.detectMultiScale(gray, 1.3, 2)
+
+    #cont faces in picture
     for (x,y,w,h) in faces:
+        #detect the rectangle coordnition 
         roi_gray = gray[y:y+h , x:x+w]
         #print(x,y,w,h)
         #recognize 
+
+        #predict the face in the rectangle
         faceID, conf = recognizer.predict(roi_gray)
         #print(conf)
-        if conf >= 5 and conf <= 80:
+
+        #if the conf is close to 0 that mean it's probley the face
+        if conf >= 5 and conf <=90:
             #print(faceID)
             #print(labels[faceID].replace("-"," ").title())
             font = cv2.FONT_HERSHEY_SIMPLEX
@@ -37,16 +65,22 @@ while True:
             color = (255, 255, 255)
             stroke = 2
             cv2.putText(img, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+            #print(str(name)+" : "+str(conf))
+
+        #if the conf is away from 90 that mean it's not the person
         else:
             font = cv2.FONT_HERSHEY_SIMPLEX
             name = "Unknown"
             color = (255, 255, 255)
             stroke = 2
             cv2.putText(img, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+            #print(str(name)+" : "+str(conf))
         #To Save The Face Just UnComment this Line and that i variable Up There in Line 12
         #roi_gray = img[y:y+h , x:x+w]
         #img_item = "img/number_"+str(i)+".png"
         #cv2.imwrite(img_item, roi_gray)
+
+        #draw the rectangle
         cv2.rectangle(img, (x,y), (x+w , y+h) , (255,0,0) , 2) 
         #i = i + 1
 
@@ -63,3 +97,6 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+#Unknown : 96.97343204015465
